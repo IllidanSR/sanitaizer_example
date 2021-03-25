3b4a5cfa3d53f0c59d807417cf148d917b46b810
illidansr <sergei.matyshev@lacit.net>
2021-03-25 09:39:01 +0300

    feat : add ub example

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <memory>
 #include <lib_a/lib_a.hpp>
 
+struct A {
+    int a;
+    int b;
+};
+
 int main() {
-    alloc_memory_not_free(10);
-    ub_example();
+    std::unique_ptr<A> v = std::make_unique<A>();
+    v->a = 4;
+    v->b = 5;
+    auto vv = std::move(v);
+    if (v) {
+        std::cout << v->a << std::endl;
+    }
+
+    /*alloc_memory_not_free(10);
+    ub_example();*/
     return 0;
 }
